0532b3ccfd9eea7493ea125a9e6a3b9486f3144e
Den71 <ancupvd@rambler.ru>
2020-11-09 23:37:26 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,110 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa988512c4ef2001e321106",
+				"5fa988512c4ef2001e321110",
+				"5fa988512c4ef2001e321108",
+				"5fa988512c4ef2001e32110a",
+				"5fa988512c4ef2001e32110c",
+				"5fa988512c4ef2001e32110e",
+				"5fa988512c4ef2001e321104",
+				"5fa988512c4ef2001e321112",
+				"5fa988512c4ef2001e321114"
+			]
+		},
+		"5fa988512c4ef2001e321104": {
+			"id": "5fa988512c4ef2001e321104",
+			"name": "404",
+			"pageUrl": "404",
+			"styles": {
+				"background": "--color-darkL2"
+			},
+			"seo": {
+				"title": "404 | Onboarding Quarkly",
+				"og:title": "404 | Onboarding Quarkly"
+			}
+		},
+		"5fa988512c4ef2001e321106": {
+			"id": "5fa988512c4ef2001e321106",
+			"name": "Intro",
+			"pageUrl": "index",
+			"seo": {
+				"title": "Intro | Onboarding Quarkly",
+				"description": "At this stage, you are going to get to know the Quarkly interface and main features.\n",
+				"og:description": "At this stage, you are going to get to know the Quarkly interface and main features.\n",
+				"og:title": "Intro | Onboarding Quarkly"
+			}
+		},
+		"5fa988512c4ef2001e321108": {
+			"id": "5fa988512c4ef2001e321108",
+			"pageUrl": "stage-2-elementary",
+			"name": "Stage 2. Hover & Adaptive",
+			"seo": {
+				"title": "Hover & Adaptive | Onboarding Quarkly",
+				"description": "At this stage, you're going to learn how to change styles for the hover state and mobile devices.\n",
+				"og:description": "At this stage, you're going to learn how to change styles for the hover state and mobile devices.\n",
+				"og:title": "Hover & Adaptive | Onboarding Quarkly"
+			}
+		},
+		"5fa988512c4ef2001e32110a": {
+			"id": "5fa988512c4ef2001e32110a",
+			"pageUrl": "stage-3-components",
+			"name": "Stage 3. Components",
+			"seo": {
+				"title": "Components | Onboarding Quarkly",
+				"og:title": "Components | Onboarding Quarkly",
+				"description": "At this stage, you're going to learn how to create custom components, update and detach them, and reset changes.\n",
+				"og:description": "At this stage, you're going to learn how to create custom components, update and detach them, and reset changes.\n"
+			}
+		},
+		"5fa988512c4ef2001e32110c": {
+			"id": "5fa988512c4ef2001e32110c",
+			"pageUrl": "stage-4-theme",
+			"name": "Stage 4. Theme",
+			"seo": {
+				"title": "Theme | Onboarding Quarkly",
+				"og:title": "Theme | Onboarding Quarkly",
+				"description": "At this stage, you're going to get to know themes, learn how to ass variables and new breakpoints to it.\n",
+				"og:description": "At this stage, you're going to get to know themes, learn how to ass variables and new breakpoints to it.\n"
+			}
+		},
+		"5fa988512c4ef2001e32110e": {
+			"id": "5fa988512c4ef2001e32110e",
+			"pageUrl": "stage-5-code-editor",
+			"name": "Stage 5. Code Editor",
+			"seo": {
+				"title": "Code Editor | Onboarding Quarkly",
+				"og:title": "Code Editor | Onboarding Quarkly",
+				"description": "At this stage, you're going to learn the basics of working in the code editor.",
+				"og:description": "At this stage, you're going to learn the basics of working in the code editor."
+			}
+		},
+		"5fa988512c4ef2001e321110": {
+			"id": "5fa988512c4ef2001e321110",
+			"pageUrl": "stage-1-basics",
+			"name": "Stage 1. Basics",
+			"seo": {
+				"title": "Basics | Onboarding Quarkly",
+				"description": "At this stage, you're going to learn how to add elements to the page, edit their content and style properties.\n",
+				"og:description": "At this stage, you're going to learn how to add elements to the page, edit their content and style properties.\n",
+				"og:title": "Basics | Onboarding Quarkly"
+			}
+		},
+		"5fa988512c4ef2001e321112": {
+			"id": "5fa988512c4ef2001e321112",
+			"pageUrl": "parrent-page",
+			"name": "Parent page"
+		},
+		"5fa988512c4ef2001e321114": {
+			"id": "5fa988512c4ef2001e321114",
+			"pageUrl": "children-page",
+			"name": "Children page"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
